fb2a4a7e787366ab031d6a56e0ce668ac87f0d67
andreiserban123 <andyspeed2003@gmail.com>
2025-03-22 16:58:57 +0200

    init

---
+++ api_oncohub_backend/src/main/java/ro/ase/ro/api_oncohub_backend/ApiOncohubBackendApplication.java
@@ -0,0 +1,13 @@
+package ro.ase.ro.api_oncohub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOncohubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOncohubBackendApplication.class, args);
+	}
+
+}
---
+++ api_oncohub_backend/src/test/java/ro/ase/ro/api_oncohub_backend/ApiOncohubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.ase.ro.api_oncohub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOncohubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
